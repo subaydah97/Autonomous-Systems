77c5df07a1095b0b8c71666bd30ee9ab842831af
1081582 <1081582@hr.nl>
2026-06-03 14:49:28 +0200

    Target abstraction layer implemented

---
+++ program_example/includes/Webots/locomotionWebots.hpp
@@ -0,0 +1,18 @@
+#include "./locomotionInterface.hpp"
+
+class LocomotionWebots
+{
+private:
+    /* data */
+public:
+    LocomotionWebots(/* args */);
+    ~LocomotionWebots();
+};
+
+LocomotionWebots::LocomotionWebots(/* args */)
+{
+}
+
+LocomotionWebots::~LocomotionWebots()
+{
+}
