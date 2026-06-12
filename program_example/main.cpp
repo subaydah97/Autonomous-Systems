724b48542e946f16e07319239b5464a3b5ca2461
1081582 <1081582@hr.nl>
2026-06-12 13:35:59 +0200

    Setup mqtt server

--- program_example/main.cpp
+++
@@ -1,5 +0,0 @@
-//Include target dependencies 
-#include "targetDependencies.hpp"
-
-//Include behaviors that extend depency interfaces
-#include "manouvering.hpp"
--- program_example/targetDependencies.hpp
+++
@@ -1,5 +0,0 @@
-/*
-This file is used to control depencies for targets.
-This paradigm allows identical main files, to be compiled for different targets.
-*/
-#include "motorWebots.hpp"
