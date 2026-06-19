4e927ef5f036706d8de20ec431b38ca66c4194a7
1081582 <1081582@hr.nl>
2026-06-19 18:28:24 +0200

    Test almost complete, chariot spawns with default data. Now to learn how to set fields

--- webot_sim_env/controllers/superVisor/superVisor.py
+++ webot_sim_env/controllers/superVisor/superVisor.py
@@ -18,7 +18,9 @@ def on_connect(client, userdata, flags, reason_code, properties):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     #print(msg.topic+" "+str(msg.payload))
-    extract_botID(msg)
+    botID = extract_botID(msg)
+    if int(botID) > 0:
+        add_chariot(msg)
     
 # Extracts the botID as integer
 def extract_botID(msg):
@@ -29,12 +31,19 @@ def extract_botID(msg):
     delim = [txt.find("/")]
     delim.append(txt.find("/", delim[-1]))
     botID = txt[delim[0]+1:]
-    print(f"{txt}  botID:({botID}) delim{delim}")
+    # print(f"botID:({botID})")
+    # print(f"delim{delim})
+    return botID
     
 # Handles adding new robots to the simulation
 def add_chariot(msg):
+    # https://cyberbotics.com/doc/reference/supervisor?tab-language=python#wb_supervisor_field_set_sf_float
+    # https://cyberbotics.com/doc/reference/supervisor?tab-language=python#wb_supervisor_field_import_mf_node_from_string
+    # See tray example provided in env folder
     print(f"Supervisor adding new robot to payload")
-    # rootChildrenField.importMFNodeFromString(-1, "{}")
+    botID = extract_botID(msg)
+    bot = rootChildrenField.importMFNodeFromString(-1, f"DEF chariot_{botID} chariot {}")
+    
     
 # create the Robot instance.
 supervisor = Supervisor()
@@ -51,11 +60,6 @@ mqttc.connect("localhost", 1883, 60)
 # Starts a background thread for managing the network loop.
 ## Prevents slowing the simulation down by multi threading. Chosen for processing speed, at th cost of required computational power.
 mqttc.loop_start()
-
-# https://cyberbotics.com/doc/reference/supervisor?tab-language=python#wb_supervisor_field_set_sf_float
-# https://cyberbotics.com/doc/reference/supervisor?tab-language=python#wb_supervisor_field_import_mf_node_from_string
-# See tray example provided in env folder
-
 # Once a new Id pops up, create a new proto with that name, set it up to be a telemetry listener
 
 
