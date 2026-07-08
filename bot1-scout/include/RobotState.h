2dae28255b1b53013402c3de4c82352d36cb03ac
subaydah <157922514+subaydah97@users.noreply.github.com>
2026-07-08 16:49:42 +0200

    Enhance RobotState with additional variables and struct/Queue
    Added new state variables and task structure for robot control.

--- bot1-scout/include/RobotState.h
+++ bot1-scout/include/RobotState.h
@@ -4,6 +4,7 @@
 
 enum RobotState
 {
+    WAITING_FOR_TARGET,
     GOING_OUT,
     GOING_HOME,
     HOME
@@ -16,18 +17,21 @@ enum class MotionMode
     BACKWARD
 };
 
-extern bool waitingForCoordinates;
-extern bool coordinatesSent;
-extern unsigned long waitStartTime;
 extern unsigned long lastTelemetryMs;
 extern bool firstTelemetryPrinted;
 extern bool telemetryEnabled;
 
-extern float latestZ;
 extern float obstacleX;
 extern float obstacleY;
+extern float latestZcm;
 extern float signedPositionTicks;
 extern float lastAvgTicksSeen;
+extern bool positionInitialized;
+
+extern float targetXcm;
+extern float targetYcm;
+extern float targetZcm;
+extern bool newObstacleTargetReceived;
 
 extern RobotState robotState;
 extern MotionMode motionMode;
@@ -46,3 +50,18 @@ extern uint32_t reverseStartLeftTicks;
 extern uint32_t reverseStartRightTicks;
 extern uint32_t forwardLeftTicks;
 extern uint32_t forwardRightTicks;
+extern bool pushingObstacle;
+extern uint32_t pushStartTime;
+
+struct Task
+{
+    float x;
+    float y;
+    float z;
+};
+
+constexpr int MAX_TASKS = 10;
+
+extern Task taskQueue[MAX_TASKS];
+extern int queueHead;
+extern int queueTail;
