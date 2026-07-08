07a4bd0b931790d9acbd98712ad8c0db5459f68d
subaydah <157922514+subaydah97@users.noreply.github.com>
2026-07-08 16:54:34 +0200

    Refactor RobotState and clean up unused variables
    Removed unused variables and the WAITING_FOR_TARGET state from RobotState enum.

--- bot1-scout/include/RobotState.h
+++ bot1-scout/include/RobotState.h
@@ -4,7 +4,6 @@
 
 enum RobotState
 {
-    WAITING_FOR_TARGET,
     GOING_OUT,
     GOING_HOME,
     HOME
@@ -17,21 +16,18 @@ enum class MotionMode
     BACKWARD
 };
 
+extern bool waitingForCoordinates;
+extern bool coordinatesSent;
+extern unsigned long waitStartTime;
 extern unsigned long lastTelemetryMs;
 extern bool firstTelemetryPrinted;
 extern bool telemetryEnabled;
 
+extern float latestZ;
 extern float obstacleX;
 extern float obstacleY;
-extern float latestZcm;
 extern float signedPositionTicks;
 extern float lastAvgTicksSeen;
-extern bool positionInitialized;
-
-extern float targetXcm;
-extern float targetYcm;
-extern float targetZcm;
-extern bool newObstacleTargetReceived;
 
 extern RobotState robotState;
 extern MotionMode motionMode;
@@ -50,5 +46,3 @@ extern uint32_t reverseStartLeftTicks;
 extern uint32_t reverseStartRightTicks;
 extern uint32_t forwardLeftTicks;
 extern uint32_t forwardRightTicks;
-extern bool pushingObstacle;
-extern uint32_t pushStartTime;
